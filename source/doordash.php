3e1558d03d2bc273e7a8ce5ed97cfc112c284d34
Josh Hurley <109998658+JoshAtDoorDash@users.noreply.github.com>
2023-02-14 16:23:30 -0500

    Added Delivery Example

--- source/doordash.php
+++ source/doordash.php
@@ -53,6 +53,39 @@ $base64UrlSignature = base64UrlEncode($signature);
 
 // Build JWT
 $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+// Unique ID that will be used for new delivery request
+$unique_id = uniqid(); // TODO: Replace with generated system ID
+
+// TODO: Update example data with actual delivery details
+// Create Delivery API Reference: https://developer.doordash.com/en-US/api/drive#tag/Delivery/operation/CreateDelivery
+$request_body = json_encode([
+    "external_delivery_id" => $unique_id,
+    "pickup_address"=> "901 Market Street 6th Floor San Francisco, CA 94103",
+    "pickup_business_name"=> "Wells Fargo SF Downtown",
+    "pickup_phone_number"=> "+16505555555",
+    "pickup_instructions"=> "Enter gate code 1234 on the callbox.",
+    "dropoff_address"=> "901 Market Street 6th Floor San Francisco, CA 94103",
+    "dropoff_business_name"=> "Wells Fargo SF Downtown",
+    "dropoff_phone_number"=> "+16505555555",
+    "dropoff_instructions"=> "Enter gate code 1234 on the callbox.",
+    "order_value"=> 1999
+  ]);
+  
+  // Build Headers for API Request
+  $headers = array(
+    "Content-type: application/json",
+    "Authorization: Bearer ".$jwt
+  );
+  
+  // Make API Request
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "https://openapi.doordash.com/drive/v2/deliveries/");
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $request_body);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  $result = curl_exec($ch);
 ?>
 <html>
 <head>
@@ -61,12 +94,17 @@ $jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
 <body>
     <h2>DoorDash API JWT</h2>
     
-    <textarea rows="4" cols="100"><?php echo $jwt; ?></textarea>
+    <div style="width: 400px; overflow-wrap: break-word; border: 1px solid black; padding: 10px"><?php echo $jwt; ?></div>
 
-    <p><b>Learn more:</b><p>
+    <h2>Create Test Order Result</h2>
+
+    <pre><code><?php echo "<script>document.write(JSON.stringify(".$result.", null, 4))</script>"; ?></code></pre>
+    
+    <h2>Learn more</h2>
     <ul>
         <li><a href="https://developer.doordash.com/en-US/docs/drive/tutorials/get_started">Get Started with the DoorDash API</a></li>
         <li><a href="https://github.com/JoshAtDoorDash/DoorDashAPI-PHP-Sample">DoorDash API PHP Sample</a></li>
     </ul>
+
 </body>
 </html>
